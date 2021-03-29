106c73722cfa2b30d4932d0462aaea5a4a269171
Evan Gruère <evan.gruere@gmail.com>
2021-03-29 10:06:43 +0200

    feat(ui): added content and graphics

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import styles from '../styles/Navbar.module.css'
 
-export default function Navbar ({ subtitles }) {
+export default function Navbar () {
 
     const [menuToggled, setMenuToggled] = useState(false)
 
@@ -13,26 +13,13 @@ export default function Navbar ({ subtitles }) {
         <div className={styles.navbar}>
             <div className={styles.navContainer}>
                 <div className={styles.navLeft}>
-                    <div className={styles.profilePicture}></div>
-                    <div className={styles.identity}>
-                        <span>Evan Gruère</span>
-                        <span className={styles.job}>Developer</span>
+                    <div className={styles.logo}></div>
+                    <div className={styles.brand}>
+                        <span>Hidavi</span>
                     </div>
-                    <div className={styles.separator}></div>
-                    <div className={`${styles.menu} ${menuToggled ? `${styles.menuActive}` : ''}`}>
-                        <div className={styles.menuItem}>Home</div>
-                        <div className={styles.menuItem}>Projects</div>
-                        <div className={styles.menuItem}>Philosophy</div>
-                    </div>
-                    <div className={styles.button}>Hire Me !</div>
                 </div>
                 <div className={styles.navRight}>
-                    <div className={styles.menuToggle}>
-                        <span className={`${styles.menuIcon} material-icons`}
-                                onClick={toggleMenu}>
-                            menu
-                        </span>
-                    </div>
+                    <div className={styles.button}>Embauchez-nous !</div>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,23 @@ export default function Home() {
     <div className={styles.container}>
       <section className={styles.section}>
         <div className={styles.content}>
-          <div className={styles.title}>My Talent. <span className={styles.callout}>Your Rules.</span></div>
-          <p>
-            Years of programming experience has led me to develop an
-            intuitive sense for solving problem on computers. As I grew older
-            with a laptop in my hands, learning the 
-            technologies that suit you and your company is just a matter of time !
-          </p>
+          <div className={styles.title}>Votre application, à vos mesures.</div>
+          <p className={styles.body}>Nous suivons les règles de design les plus modernes pour offrir à vos utilisateurs une navigation éloquente. Tout au long du processus de création, nous recueillons vos suggestions afin de parfaire l’expérience finale.</p>
         </div>
         <div className={styles.illustration}>
-          <div className={styles.code}></div>
-          <div className={styles.space}></div>
-          <div className={styles.spaceship}></div>
-          <div className={styles.spaceOutline}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.window}></div>
+        </div>
+      </section>
+      <div className={styles.wave}></div>
+      <section className={styles.lightSection}>
+        <div className={styles.content}>
+          <div className={styles.title}>Un coup de projecteur sur votre présence en ligne.</div>
+          <p className={styles.body}>Nous suivons les règles de design les plus modernes pour offrir à vos utilisateurs une navigation éloquente. Tout au long du processus de création, nous recueillons vos suggestions afin de parfaire l’expérience finale.</p>
+        </div>
+        <div className={styles.illustration}>
+          <div className={styles.grid}></div>
+          <div className={styles.window}></div>
         </div>
       </section>
     </div>
